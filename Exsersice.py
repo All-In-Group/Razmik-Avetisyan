3fb4aa2abd7a3583ae5bb0fff0e9f4968206b3c4
Razmik Avetisyan <polygon86.86@gmail.com>
2020-10-24 16:03:15 +0400

    Exsersice 2

--- Exsersice.py
+++ Exsersice.py
@@ -1,3 +1,4 @@
+#Excersie 1.1
 str1 = "JohnDipPeta"
 newstr1 = list(str1)
 output = None
@@ -5,6 +6,14 @@ for i in range (0,len(newstr1)):
 	if(newstr1[i]=="D" and newstr1[i+1]=='i' and newstr1[i+2]=='p'):
 		output = newstr1[i]+ newstr1[i+1]+newstr1[i+2]
 print(output)
+
+#Excersie 2
+#Given 2 strings, s1 and s2, create a new string by appending s2 in the middle of s1
+s1 = "Ault"
+s2 = "Kelly"
+result=s1[0:2]+s2+s1[2:4]
+print(result)
+
 
 
 
