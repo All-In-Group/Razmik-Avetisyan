f59f5f199312470b5fcb4f219da633d3a0bf59b9
Razmik Avetisyan <polygon86.86@gmail.com>
2020-11-04 00:46:54 +0400

    Add Loops Exersises

--- Loops/Loops.py
+++ Loops/Loops.py
@@ -9,3 +9,13 @@ for i in range(0,6):
 for num in range(-10,0):
 	print(num)
 
+#Question 11:Use a loop
+# to display elements from a given
+# list which are present at even positions
+var = 75642
+listVar = str(var)
+for j in range(len(listVar)):
+	print(listVar[j])
+
+
+
